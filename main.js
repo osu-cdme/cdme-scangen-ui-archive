999f54221bee881c69487bce1087d7b3c79c871e
Anden Acitelli <a.acitelli@gmail.com>
2021-07-03 16:26:40 -0400

    Overall page layout done; now for actually pulling the data in

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow () {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600, 
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
 }
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
 
 app.whenReady().then(() => {
     createWindow()
