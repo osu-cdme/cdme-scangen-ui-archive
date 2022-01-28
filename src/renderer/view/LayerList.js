681a7395bc577c9f347a61d2f9fff0b4d2f4514e
Anden Acitelli <a.acitelli@gmail.com>
2022-01-28 09:32:53 -0500

    Bugfix: Layers not correctly labeled if absolute path to layer has numbers in it

--- src/renderer/view/LayerList.js
+++ src/renderer/view/LayerList.js
@@ -1,57 +1,62 @@
 // PURPOSE: Handle everything about the Layer List
 
-const natsort = require('natsort').default;
-const { fs, path, paths, d3 } = require('../common');
-const { drawBuild, drawBuildCanvas } = require('./drawing');
-const { getBuildFromFilePath, setCurrentBuild, setCurrentPath } = require('../common');
+const natsort = require("natsort").default;
+const { fs, path, paths, d3 } = require("../common");
+const { drawBuild, drawBuildCanvas } = require("./drawing");
+const { getBuildFromFilePath, setCurrentBuild, setCurrentPath } = require("../common");
 
-const glob = require('glob');
+const glob = require("glob");
 module.exports.RenderLayerList = () => {
-  return new Promise((resolve, reject) => {
-    const files = glob.sync(path.join(paths.GetUIPath(), 'xml', '*.xml')).sort(natsort());
-    let numDrawn = 0;
-    files.forEach(async file => {
-      const filePath = file;
-      const layerNum = parseInt(file.match(/(\d+)/)[0]); // First part finds the number, second part trims zeroes
+    return new Promise((resolve, reject) => {
+        const files = glob.sync(path.join(paths.GetUIPath(), "xml", "*.xml")).sort(natsort());
+        let numDrawn = 0;
+        files.forEach(async (file) => {
+            const filePath = file;
 
-      const li = d3.select('#layerList').append('li');
+            // Just to make sure we don't match numbers in the filepath or something, we do what's essentially a nested regex search
+            const endpart = file.match(/scan_\d+/g)[0];
+            const layerNum = parseInt(endpart.match(/\d+/g)[0]);
 
-      li.append('canvas')
-        .attr('id', 'canvas_' + layerNum)
-        .attr('width', 50)
-        .attr('height', 50);
-      const build = await getBuildFromFilePath(filePath);
-      drawBuildCanvas(build, 'canvas_' + layerNum);
+            const li = d3.select("#layerList").append("li");
 
-      if (layerNum === 1) {
-        li.classed('selectedLayer', true);
-      }
+            li.append("canvas")
+                .attr("id", "canvas_" + layerNum)
+                .attr("width", 50)
+                .attr("height", 50);
+            const build = await getBuildFromFilePath(filePath);
+            drawBuildCanvas(build, "canvas_" + layerNum);
 
-      li.append('p')
-        .text('Layer ' + layerNum) // Extract only the number from the file name
-        .on('click', async e => {
-          wipeSelectedLayers();
-          li.classed('selectedLayer', true);
-          e.preventDefault();
-          const build = await getBuildFromFilePath(filePath);
-          setCurrentBuild(build);
-          setCurrentPath(filePath);
-          drawBuild(build, 'mainsvg');
+            if (layerNum === 1) {
+                li.classed("selectedLayer", true);
+            }
+
+            li.append("p")
+                .text("Layer " + layerNum) // Extract only the number from the file name
+                .on("click", async (e) => {
+                    wipeSelectedLayers();
+                    li.classed("selectedLayer", true);
+                    e.preventDefault();
+                    const build = await getBuildFromFilePath(filePath);
+                    setCurrentBuild(build);
+                    setCurrentPath(filePath);
+                    drawBuild(build, "mainsvg");
+                });
+
+            // Update loading screen based on how far in we are
+            numDrawn++;
+            const progress = Math.floor((numDrawn / files.length) * 100);
+            document.getElementById("loading").textContent = `Loading. Please wait a sec... (Progress: ${progress}%)`;
+            if (numDrawn === files.length) {
+                resolve();
+            }
         });
-
-      // Update loading screen based on how far in we are
-      numDrawn++;
-      const progress = Math.floor(numDrawn / files.length * 100);
-      document.getElementById('loading').textContent = `Loading. Please wait a sec... (Progress: ${progress}%)`;
-      if (numDrawn === files.length) {
-        resolve();
-      }
     });
-  });
 };
 
-function wipeSelectedLayers () {
-  d3.select('#layerList').selectAll('li').each(function () {
-    d3.select(this).classed('selectedLayer', false);
-  });
+function wipeSelectedLayers() {
+    d3.select("#layerList")
+        .selectAll("li")
+        .each(function () {
+            d3.select(this).classed("selectedLayer", false);
+        });
 }
